268271b7b18aea9e9e37e669c70bfa9c588498e0
Nikolay Varbanov <nikolayvurbanov86@gmail.com>
2025-01-16 16:21:29 +0100

    1) Added the Order class which holds all the elements of a meal 2) Be aware that the Order IS responsible for the deallocation of the order parts. Even though the Order didn't allocate them (The future Builder will allocate them)

--- CApp.cpp
+++ CApp.cpp
@@ -20,64 +20,29 @@
 #include "ActionFigure.h"
 #include "SportToy.h"
 
+#include "Order.h"
+
 int main() {
 
 	std::cout << "Hey Im Working here!!!" << std::endl;
-
-	std::cout << "Main Meal" << std::endl;
+	
+	std::cout << "Order" << std::endl;
 	
 	MainMeal * hamburger = new Hamburger();
-	MainMeal * hotDog = new HotDog();
-	MainMeal * cheeseBurger = new CheeseBurger();
-	
-	hamburger -> OnRender();
-	hotDog -> OnRender();
-	cheeseBurger -> OnRender();
-	
-	delete hamburger;
-	delete hotDog;
-	delete cheeseBurger;
-	
-	std::cout << "Side Meal" << std::endl;
 	
 	SideMeal * frenchFries = new FrenchFries();
-	SideMeal * onionRings = new OnionRings();
-	SideMeal * chikenChunks = new ChikenChunks();
-	
-	frenchFries -> OnRender();
-	onionRings -> OnRender();
-	chikenChunks -> OnRender();
-	
-	delete frenchFries;
-	delete onionRings;
-	delete chikenChunks;
-	
-	std::cout << "Drinks" << std::endl;
 	
 	Drinks * cola = new Cola();
-	Drinks * soda = new Soda();
-	Drinks * fanta = new Fanta();
-	
-	cola -> OnRender();
-	soda -> OnRender();
-	fanta -> OnRender();
-	
-	delete cola;
-	delete soda;
-	delete fanta;
-	
-	std::cout << "Toy" << std::endl;
 	
 	Toy * car = new Car();
-	Toy * actionFigure = new ActionFigure();
-	Toy * sportToy = new SportToy();
 	
-	car -> OnRender();
-	actionFigure -> OnRender();
-	sportToy -> OnRender();
+	Order * order = new Order ( hamburger,
+															frenchFries,
+															cola,
+															car );
 	
-	delete car;
-	delete actionFigure;
-	delete sportToy;
+	order -> OnRender();
+	
+	delete order;
 	
 }
---
+++ Order.h
@@ -0,0 +1,49 @@
+#ifndef _ORDER_H
+#define _ORDER_H
+
+#include <iostream>
+
+#include "MainMeal.h"
+#include "SideMeal.h"
+#include "Drinks.h"
+#include "Toy.h"
+
+class Order {
+	public: // Constructs
+		Order(MainMeal * tmpMainMeal,
+					SideMeal * tmpSideMeal,
+					Drinks * tmpDrinks,
+					Toy * tmpToy) : _mainMeal(tmpMainMeal),
+													_sideMeal(tmpSideMeal),
+													_drinks(tmpDrinks), 
+													_toy(tmpToy) { 
+			std::cout << "Constructing an Order" << std::endl; 
+				};
+		~Order() { 
+			std::cout << "Destructiong an Order" << std::endl; 
+			
+			delete _mainMeal;
+			delete _sideMeal;
+			delete _drinks;
+			delete _toy;
+			
+			};
+	public: // Facilities
+		MainMeal * _mainMeal;
+		SideMeal * _sideMeal;
+		Drinks * _drinks;
+		Toy * _toy;
+	public: // Utilities
+		void OnRender() {
+			std::cout << "Order has: "
+								<< std::endl;
+			
+			_mainMeal -> OnRender();
+			_sideMeal -> OnRender();
+			_drinks -> OnRender();
+			_toy -> OnRender();
+			
+		};
+};
+
+#endif
